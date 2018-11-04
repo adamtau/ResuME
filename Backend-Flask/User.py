b997b2ef794a56c3e5c40348ba3ded5bb3c8e334
Adam Tao <adamtao@berkeley.edu>
2018-11-03 22:18:00 -0700

    modified User

--- Backend-Flask/User.py
+++ Backend-Flask/User.py
@@ -1,14 +1,18 @@
+
 class UserProfile(object):
+
     #[_ID, "first name", "last name", "Description", "Ethnicity", "Phone Number", "Current Address"
     #"Experiences": [list of strings], "Education": [list of dictionaries]
     #"Summary", "Courses", "Languages", "Skills", ]
 
     #Experiences: [Title: , Company: , Location: , "" Start Date (Month, year): , End Date: ,UploadFiles, Links, WorkHere]
     #Educations: [School:, Degree, Field of Study, Grade, Activities and Societies:, From Year, to Year, Description]
-    def __init__(self, bfemail, bfpassword, first_name, last_name, major, description = "", ethnicity = "", currentAddress = "", summary = "", 
-                experiences = [], educations = [], courses = [], languages = [], skills = []):
+    def __init__(self, bfemail, bfpassword, alemail, alpassword, first_name, last_name, major, description = "", ethnicity = "", currentAddress = "", summary = "",
+                experiences = [], educations = [], courses = [], languages = [], skills = [], projects=[]):
         self.bfemail = bfemail
         self.bfpassword = bfpassword
+        self.alemail = alemail
+        self.alpassword = alpassword
         self.profile = {}
         self.profile['first_name'] = first_name
         self.profile['last_name'] = last_name
@@ -20,7 +24,7 @@ class UserProfile(object):
         self.profile['experiences'] = experiences
         self.profile['number_of_experiences'] = len(experiences)
         self.profile['education'] = educations
-        self.profile['number_of_educations'] = len(education)
+        self.profile['number_of_educations'] = len(educations)
         self.profile['courses'] = courses
         self.profile['number_of_courses'] = len(courses)
         self.profile['languages'] = languages
@@ -29,6 +33,8 @@ class UserProfile(object):
         self.profile['number_of_skills'] = len(skills)
         self.profile['bfemail'] = bfemail
         self.profile['bfpassword'] = bfpassword
+        self.profile['projects'] = projects
+        self.profile['number_of_projects'] = len(projects)
 
 class Address(object):
     def __init__(self, street, street_number, state, country, zip_code):
@@ -37,6 +43,7 @@ class Address(object):
         self.state = state
         self.country = country
         self.zip_code = zip_code
+
 
 class Education(object):
     def __init__(self, school, degree, field_of_study, grade, activities_societies, from_year,
@@ -50,17 +57,18 @@ class Education(object):
         self.to_year = to_year
         self.description = description
 
+
 class Experience(object):
-    def __init__(self, title, company, location, start_date, end_date, uploadFiles = [], links = [], description = "", workhere = "False"):
+    def __init__(self, title, company, location, start_date, end_date, links = [], description = "", workhere = "False"):
         self.title = title
         self.company = company
         self.location = location
         self.start_date = start_date
         self.end_date = end_date
-        self.uploadFiles = uploadFiles
         self.links = links
         self.workhere = workhere
         self.description = description
+
 
 class Links(object):
     def __init__(self, personal_website, linkedin, github, stackOverflow):
@@ -68,3 +76,15 @@ class Links(object):
         self.linkedin = linkedin
         self.github = github
         self.stackOverflow = stackOverflow
+
+class Project(object):
+    def __init__(self, title, role, summary, start_time, end_time="", description="", url="", Organization="", currentWorking=False):
+        self.title = title
+        self.role = role
+        self.summary = summary
+        self.description = description
+        self.link = url
+        self.organization = Organization
+        self.start_date = start_time
+        self.end_date = end_time
+        self.currentWorking = currentWorking
